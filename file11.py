30b681b4e92ad8f252f664f8a195fee83f75ee55
yun-21 <75606522+yun-21@users.noreply.github.com>
2022-04-02 23:10:15 +0900

    Update file11.py

--- file11.py
+++ file11.py
@@ -2,8 +2,16 @@ import sys
 myF = open("file.txt", "w")
 myF.write("Opens a file for reading only in binary.\n")
 myF.write("The file pointer is placed.\n")
-myF.write("This is the default mode.")
+myF.write("This is the default mode.\n")
+d = input("기본적인 내용말고도 더 적고 싶은 내용이 있습니까?(Y/N):")
+while d=="Y":
+    if d=="Y":
+        add = input("적으시오: ")
+        myF.write(add+"\n")
+    d = input("더 적고 싶습니까?(Y/N):")
+
 myF.close()
+
 
 try:
     user = input("요청 할 파일이름을 적으시오: ")
